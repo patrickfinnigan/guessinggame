1454401c2eac3ba98d8d313e8a92c2899ab9dc08
thebreezyp <patrickfinnigan94@gmail.com>
2018-06-05 20:25:25 -0700

    adjustments to js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,43 +2,51 @@ var alaphabet = "abcdefghijklmnopqrstuvwxyz";
 var wins = 0;
 var losses = 0;
 var left = 10;
+var guesses = ''
 
-document.onkeyup = function() {
-    var userguess = event.keyCode;
-    if (userguess >= 65 && userguess <= 90 && event.shiftKey === false) {
-        console.log("true")
+var computerGuess = alaphabet[Math.floor(Math.random() * alaphabet.length)];
+console.log(computerGuess)
+
+document.onkeyup = function (event) {
+    var userguess = event.key;
+    if (event.keyCode >= 65 && event.keyCode <= 90 && event.shiftKey === false) {
+      console.log("true")
     } else {
-        console.log("false")
+        alert("Please Pick A Letter!")
     }
-    console.log(userguess);
-
-    var userguess = '';
-
-    var computerGuess = alaphabet[Math.floor(Math.random()*alaphabet.length)];
-    console.log(computerGuess)
 
 
     if (userguess == computerGuess) {
-        wins + 1;
+        wins += 1;
+        alert("You Win!")
+        computerGuess = alaphabet[Math.floor(Math.random() * alaphabet.length)];
+        console.log(computerGuess)
+        left = 10;
+        guesses = ''
     } else {
         left = left - 1;
+        guesses += userguess + ' '
     }
 
     if (left == 0) {
-        losses + 1;
+        alert("Game Over")
+        computerGuess = alaphabet[Math.floor(Math.random() * alaphabet.length)];
+        console.log(computerGuess)
+        losses += 1;
         left = 10;
+        guesses = ''
     } else {
         console.log(left)
     }
 
 
 
-    var html= "<h1>The Psychic Game</h1>" +
-    "<h3>Guess What Letter I'm Thinking Of</h3>" +
-    "<h3>Wins: " + wins + "</h3>" +
-    "<h3>Losses: " + losses + "</h3>" +
-    "<h3>Guesses Left: " + left + "</h3>";
-    // "<h3>Your Guesses So Far " + guesses + "</h3>";
+    var html = "<h1>The Psychic Game</h1>" +
+        "<h3>Guess What Letter I'm Thinking Of</h3>" +
+        "<h3>Wins: " + wins + "</h3>" +
+        "<h3>Losses: " + losses + "</h3>" +
+        "<h3>Guesses Left: " + left + "</h3>" +
+        "<h3>Your Guesses So Far: " + guesses + "</h3>";
 
     document.querySelector("#game").innerHTML = html;
 
